97bc51a2fc4a6e604730d3fee17b31783bc1c06c
kouta1612 <kouta1612world69@gmail.com>
2019-05-06 14:58:08 +0900

    complete #33

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,15 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <slot name="menu-list" :item="item">{{ item }}</slot>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  props: ["items"]
+};
 </script>
